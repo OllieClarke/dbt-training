4238aaadeb86cadf1a470c0f51f09c336bc6878d
Ollie Clarke <oliver.clarke@theinformationlab.co.uk>
2024-12-03 17:53:04 +0000

    creating a snapshot

---
+++ analyses/inspecting_snapshot.sql
@@ -0,0 +1,2 @@
+select * from dbt.dbt_oclarke_snapshot.mock_orders
+order by order_id, dbt_updated_at
---
+++ snapshots/mock_orders.sql
@@ -0,0 +1,16 @@
+{% snapshot mock_orders %}
+{% set new_schema = target.schema + '_snapshot' %}
+
+    {{
+        config(
+            target_schema=new_schema,
+            target_database='dbt',
+            unique_key='order_id',
+            
+            strategy='timestamp',
+            updated_at='updated_at'
+        )
+    }}
+
+    select * from dbt.{{target.schema}}.mock_orders
+ {% endsnapshot %}
