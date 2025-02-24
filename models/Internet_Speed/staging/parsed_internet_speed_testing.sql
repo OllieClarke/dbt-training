55ff6afa0801659798e1d9509822fa8d35da70a1
Ollie Clarke <oliver.clarke@theinformationlab.co.uk>
2025-02-24 16:38:53 +0000

    removed internet speed as is being done elsewhere

--- models/Internet_Speed/staging/parsed_internet_speed_testing.sql
+++
@@ -1,6 +0,0 @@
-select
-    json:Download::Float / 1000000 as Download
-    , json:Upload::Float / 1000000 as Upload
-    , json:Ping::Float as Ping
-    , json:Timestamp::Timestamp as Timestamp
-from {{ ref('src__internet_speed_testing') }}
